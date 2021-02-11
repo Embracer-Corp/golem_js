58b5e48020413d4bb9028617c661aca358d8b163
AndreevAV <AndreevAndreiV@gmail.com>
2021-02-12 02:16:37 +0300

    add hsv color support, step #11

--- public/basic.js
+++ public/basic.js
@@ -4,9 +4,11 @@ const canvasContext = canvas.getContext("2d")
 var fps = 1000/100
 var lps = 1000/100
 
+// ----- Math -----
 function lerp(v1, v2, w) {
   return v1 + w * (v2 - v1)
 }
+// ----- Vector -----
 function normalize(vector, scale) {
   var norm = Math.sqrt(vector.x * vector.x + vector.y * vector.y)
   if (norm != 0) {
@@ -18,6 +20,79 @@ function multiplication(vector, scale) {
   vector.x *= scale
   vector.y *= scale
 }
+// ----- Colors -----
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+function componentToHex(c) {
+  if (c <= 1) { c = Math.round(c*255) }
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+  if (typeof(r) == "object") {
+    return rgbToHex(r.r?r.r:r.h, r.g?r.g:r.s, r.b?r.b:r.v)
+  } else return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+function rgbToHsv(r, g, b) {
+  if (typeof(r) == "string") {
+    // console.dir(hexToRgb(r))
+    return rgbToHex(rgbToHsv(hexToRgb(r)))
+  } else if (typeof(r) == "object") {
+    return rgbToHsv(r.r, r.g, r.b)
+  } else if (typeof(r) != "number" || typeof(g) != "number" || typeof(b) != "number") {
+    return null
+  }
+  
+  var max = Math.max(r, g, b), min = Math.min(r, g, b),
+      d = max - min,
+      h,
+      s = (max === 0 ? 0 : d / max),
+      v = max / 255
+
+  switch (max) {
+      case min: h = 0; break;
+      case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
+      case g: h = (b - r) + d * 2; h /= 6 * d; break;
+      case b: h = (r - g) + d * 4; h /= 6 * d; break;
+  }
+
+  return { h: h, s: s, v: v }
+}
+
+function hsvToRgb(h, s, v) {
+  if (typeof(h) == "string") {
+    return rgbToHex(hsvToRgb(hexToRgb(h)))
+  } else if (typeof(h) == "object") {
+    return hsvToRgb(h.h?h.h:h.r, h.s?h.s:h.g, h.v?h.v:h.b)
+  } else if (typeof(h) != "number" || typeof(s) != "number" || typeof(v) != "number") {
+    return null
+  }
+
+  var r, g, b,
+      i = Math.floor(h * 6),
+      f = h * 6 - i,
+      p = v * (1 - s),
+      q = v * (1 - f * s),
+      t = v * (1 - (1 - f) * s)
+
+  switch (i % 6) {
+      case 0: r = v, g = t, b = p; break;
+      case 1: r = q, g = v, b = p; break;
+      case 2: r = p, g = v, b = t; break;
+      case 3: r = p, g = q, b = v; break;
+      case 4: r = t, g = p, b = v; break;
+      case 5: r = v, g = p, b = q; break;
+  }
+  return { r: Math.round(r * 255), g: Math.round(g * 255), b: Math.round(b * 255) }
+}
+
+
 
 const general = {
   fps: 1000/100,
